2e2ffd8078f92620c5f45d4335676407b9f882dd
Chas Huggins <hugginsc10@gmail.com>
2024-03-27 18:53:19 -0400

    version 0.1.1

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 setup(
     name="uf-toolkit",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     description="A simple Union Find package for those that don't want to come up with the UnionFind Class and Methods on the spot.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Chas Huggins",
     author_email="hugginsc10@gmail.com",
     url="https://github.com/hugginsc10/uf-toolkit",
